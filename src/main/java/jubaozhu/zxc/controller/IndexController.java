b1dc34cac0dbffe2fa7d868c2b9dbeb43db97bb9
kadouzhyu <kadouzhyu@163.com>
2018-06-13 22:18:15 +0800

    新改啦

--- src/main/java/jubaozhu/zxc/controller/IndexController.java
+++ src/main/java/jubaozhu/zxc/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController
 {
-  //首页
+  //首页 哈
   @RequestMapping({"/"})
   public String  index()
   {
